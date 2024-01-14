033a94a9d9e6a14df20a22597dfbb444fac1293c
Kseniya <kkosnyreva@digimatics.ru>
2024-01-14 21:35:54 +0300

    add 3 mrthod with annotation and param

--- src/test/java/ksyu/kos/SimpleHomeTest.java
+++ src/test/java/ksyu/kos/SimpleHomeTest.java
@@ -6,14 +6,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 import java.util.stream.Stream;
 
-import static com.codeborne.selenide.CollectionCondition.texts;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.empty;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class SimpleHomeTest {
@@ -34,13 +37,31 @@ public class SimpleHomeTest {
 
     @ParameterizedTest
     @Tag("SMOKE")
-    @DisplayName("Проверка наличия топ-5 услуг в блоке поиска на главной странице и доступности")
+    @DisplayName("При выборе категории из топ-5 должен отображаться соответствующий список услуг")
     @MethodSource
     void findServiceLabelAndNavigateToChat(String serviceLabel, List<String> dialogMenu) {
-        $$(".ng-star-inserted .preset__label").findBy(text(serviceLabel)).click();
+        $$("portal-plain-button.ng-star-inserted").findBy(text(serviceLabel)).click();
         $$("portal-search-message-button.ng-star-inserted").
                 filterBy(visible).
                 shouldHave(texts(dialogMenu));
+    }
+
+    @ParameterizedTest
+    @ValueSource (strings = {"Популярные услуги","Здоровье","Справки Выписки"})
+    @Tag("SMOKE")
+    @DisplayName("При клике на категорию услуги должно открыться всплывающее окно с описанием")
+    void serviceCategoryShouldHaveFullDescription (String category) {
+        $("lib-cat-nav-categories div").$$("button").findBy(attribute("aria-label", category)).click();
+        $("lib-cat-nav-tab.ng-star-inserted").shouldNotBe(empty);
+    }
+
+    @ParameterizedTest(name = "При выборе группы {0} топ лист услуг должен иметь заданный размер {1}")
+    @CsvFileSource(resources = {"/testdata/targetGroupShouldHaveTopListServices.csv"},delimiter = '|')
+    @Tag("SMOKE")
+    void GroupShouldHaveFixedAmountServices (String targetGroup, int amountServices) {
+        $("lib-header-select-role.ng-star-inserted").click();
+        $(".list.ng-star-inserted").$$("button.ng-star-inserted").findBy(text(targetGroup)).click();
+        $$("portal-plain-button.ng-star-inserted").shouldHave(size(amountServices));
     }
 
 }
