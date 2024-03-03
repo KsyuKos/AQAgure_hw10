9e08af3ac6329aed0cc09744527ceea3346aa0fb
Kseniya <kkosnyreva@digimatics.ru>
2024-03-04 01:10:37 +0300

    fixes

--- src/test/java/ksyu/kos/ReadFileFromZip.java
+++ src/test/java/ksyu/kos/ReadFileFromZip.java
@@ -1,25 +1,37 @@
 package ksyu.kos;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class ReadFileFromZip {
     private final ClassLoader cl = ReadFileFromZip.class.getClassLoader();
-    private String path;
 
-    public ReadFileFromZip(String string) {
-        this.path = string;
-    }
+    public ZipEntry streamZips(String fileName, String zipFilePath) throws Exception {
+        try (InputStream getZip = cl.getResourceAsStream(zipFilePath); ZipInputStream zipStream = new ZipInputStream(getZip)) {
+            ZipEntry file = zipStream.getNextEntry();
+            int n = 0;
 
-    public void streamZip () throws Exception {
-        try (InputStream getZip = cl.getResourceAsStream(path);
-             ZipInputStream zipStream = new ZipInputStream(getZip)) {
-            ZipEntry file=zipStream.getNextEntry();
-                System.out.println(file.getName());
+            while (file != null) {
+                if (file.getName().equals(fileName)) {
+                     return file;
+                }
+                else {throw new IOException("Файла нет в архиве");}
+            }
+
         }
+        return null;
     }
 
-
+    public static void main(String[] args) throws Exception {
+        ReadFileFromZip art = new ReadFileFromZip();
+        art.streamZips("test.csv","testdata/archive.zip");
+        //System.out.println;
+    }
 
 }
+
+
+
+
--- src/test/java/ksyu/kos/SelenideFileTest.java
+++ src/test/java/ksyu/kos/SelenideFileTest.java
@@ -3,15 +3,14 @@ package ksyu.kos;
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.opencsv.CSVReader;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -19,6 +18,7 @@ import java.util.zip.ZipInputStream;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.*;
 
 public class SelenideFileTest {
 
@@ -44,55 +44,92 @@ public class SelenideFileTest {
     private final ClassLoader cl = SelenideFileTest.class.getClassLoader();
 
     @Test
-    void checkFileFromZip() throws Exception {
+    @DisplayName("Проверка CSV файла")
+    void checkCsvFileFromZip() throws Exception {
         try (InputStream getZip = cl.getResourceAsStream("testdata/archive.zip");
              ZipInputStream zipStream = new ZipInputStream(getZip);
              CSVReader csvReader = new CSVReader(new InputStreamReader(zipStream))) {
 
             ZipEntry file;
+            int n = 0;
 
             while ((file = zipStream.getNextEntry()) != null) {
-                //byte[] byteFile = zipStream.readAllBytes();
-                //String byteFileToString = new String(byteFile, UTF_8);
 
-                if (file.getName().equals("csv.csv")) {
+                if (file.getName().equals("test.csv")) {
                     List<String[]> contentCsv = csvReader.readAll();
-                    Assertions.assertArrayEquals(
-                            new String[]{"title", "artist", "album", "isrc", "isFound"}, contentCsv.get(0)
+                    assertThat(contentCsv.get(0)).isEqualTo(
+                            new String[]{"title", "artist", "album", "isrc", "isFound"}
                     );
-                } else if (file.getName().equals("pdf.pdf")) {
-                    PDF contentPdf = new PDF(zipStream);
-                    Assertions.assertEquals(4, contentPdf.numberOfPages);
-                } else if (file.getName().equals("xlsx.xlsx")) {
-                    XLS contentXlsx = new XLS(zipStream);
-                    Assertions.assertEquals("test2",
-                                    contentXlsx.
-                                            excel.
-                                            getSheet("List").
-                                            getRow(1).
-                                            getCell(1).
-                                            getStringCellValue());
+                    n++;
                 }
-
+            }
+            if (n == 0){
+                throw new IOException("Файла нет в архиве");
             }
         }
     }
 
     @Test
-    void checkJsonTest () throws Exception {
-        InputStream getJson = cl.getResourceAsStream("testdata/json.json");
+    @DisplayName("Проверка PDF файла")
+    void checkPdfFileFromZip() throws Exception {
+        try (InputStream getZip = cl.getResourceAsStream("testdata/archive.zip");
+             ZipInputStream zipStream = new ZipInputStream(getZip)) {
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            Account objectAccount = objectMapper.readValue(getJson, Account.class);
-            Assertions.assertEquals(objectAccount.getStatus(), "ACTIVE");
-            Assertions.assertEquals(objectAccount.getTransactions().getAmount(), 3000);
-            Assertions.assertEquals(objectAccount.getRemote_accounts().get(0).getBalance(), 1000);
-            Assertions.assertArrayEquals(new Integer[]{1000,2000}, objectAccount
-                    .getRemote_accounts()
-                    .stream()
-                    .map(RemoteAccounts::getBalance)
-                    .toArray());
+            ZipEntry filePdf;
+
+            while ((filePdf = zipStream.getNextEntry()) != null) {
+
+                if (filePdf.getName().equals("pdf.pdf")) {
+                    PDF contentPdf = new PDF(zipStream);
+                    assertThat(contentPdf.numberOfPages).isEqualTo(4);
+                }
+            }
         }
     }
+
+    @Test
+    @DisplayName("Проверка XSLX файла")
+    void checkXlsxFileFromZip() throws Exception {
+        try (InputStream getZip = cl.getResourceAsStream("testdata/archive.zip");
+             ZipInputStream zipStream = new ZipInputStream(getZip)) {
+
+            ZipEntry fileXlsx;
+
+            while ((fileXlsx = zipStream.getNextEntry()) != null) {
+
+                if (fileXlsx.getName().equals("xlsx.xlsx")) {
+                    XLS contentXlsx = new XLS(zipStream);
+                    assertThat(contentXlsx.
+                                    excel.
+                                    getSheet("List").
+                                    getRow(1).
+                                    getCell(1).
+                                    getStringCellValue()).isEqualTo("test2");
+                }
+            }
+        }
+    }
+
+
+    @Test
+    @DisplayName("Проверка JSON файла")
+    void checkJsonTest() throws Exception {
+        InputStream getJson = cl.getResourceAsStream("testdata/json.json");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Account objectAccount = objectMapper.readValue(getJson, Account.class);
+        assertThat(objectAccount.getStatus()).isEqualTo("ACTIVE");
+        assertThat(objectAccount.getTransactions().getAmount()).isEqualTo(3000);
+        assertThat(objectAccount.getRemote_accounts().get(0).getBalance()).isEqualTo(1000);
+        assertThat(objectAccount
+                .getRemote_accounts()
+                .stream()
+                .map(RemoteAccounts::getBalance)
+                .toArray()).isEqualTo(new Integer[]{1000, 2000});
+    }
+
+
+
+}
 
 
